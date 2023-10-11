7d17d179d26cb4cf80f614078cb94d07e8bd3d10
Szymon Michna <szymek0705@gmail.com>
2023-10-11 20:36:33 +0200

    adding a two-way swap

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 print("Change yout mac address from : to -")
 
 def change_mac(mac_address):
-    return mac_address.replace(":","-")
+    if third_character == ":":
+        return mac_address.replace(":", "-")
+    else:
+        return mac_address.replace("-", ":")
 
 mac_address = input("Paste your mac address: ")
+third_character = mac_address[2]
 mac_new = change_mac(mac_address)
 print("Your mac address after change: ", mac_new)
