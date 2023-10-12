70b49be89ba8af4f9fe3778e9626f7fa88cf1328
Szymon <szymek0705@gmail.com>
2023-10-12 15:51:52 +0200

    add color to text

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pyperclip
 
+class color:
+    new = '\033[94m'
+    ENDC = '\033[0m'
+    WARNING = '\033[91m'
+    GREEN = '\033[92m'
+
 while True:
     print("Change your mac address from : to - and - to :")
 
@@ -16,12 +22,12 @@ while True:
     mac_address = input("Paste your mac address: ")
     third_character = mac_address[2]
     mac_new = change_mac(mac_address)
-    print("Your mac address after change: ", mac_new)
+    print("Your mac address after change: ",color.new +  mac_new + color.ENDC)
     pyperclip.copy(mac_new)
     print("Your mac address after change is in on clipboard")
 
     while True:
-        answer = str(input('Run again? (y/n): '))
+        answer = str(input('Run again? ('+color.GREEN+'y'+color.ENDC+'/'+color.WARNING+' n'+color.ENDC+'): '))
         if answer in ('y', 'n'):
             break
         print("Invalid input")
